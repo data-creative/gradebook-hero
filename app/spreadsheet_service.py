510855553635c36abdfa2fb350a9618225f7019e
Chris Delaney <96562952+chrissdelaney@users.noreply.github.com>
2024-02-11 19:25:03 -0500

    getting courses draft

--- app/spreadsheet_service.py
+++ app/spreadsheet_service.py
@@ -14,6 +14,8 @@ from dotenv import load_dotenv
 import gspread
 from gspread.exceptions import SpreadsheetNotFound
 
+import time
+
 
 load_dotenv()
 
@@ -21,7 +23,6 @@ DEFAULT_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "gradebook-hero
 GOOGLE_CREDENTIALS_FILEPATH = os.getenv("GOOGLE_CREDENTIALS_FILEPATH", default=DEFAULT_FILEPATH)
 
 GOOGLE_SHEETS_MASTER_DOCUMENT_ID = os.getenv("GOOGLE_SHEETS_MASTER_DOCUMENT_ID", default="OOPS Please get the spreadsheet identifier from its URL, and set the 'GOOGLE_SHEETS_DOCUMENT_ID' environment variable accordingly...")
-MASTER_ROSTER_DOCUMENT_ID = os.getenv("MASTER_ROSTER_DOCUMENT_ID")
 
 
 class SpreadsheetService:
@@ -49,6 +50,9 @@ class SpreadsheetService:
         """note: this will make an API call each time, to get the new data"""
         return self.client.open_by_key(self.document_id) #> <class 'gspread.models.Spreadsheet'>
 
+    def set_active_document(self, document_id):
+        self.document_id = document_id
+
     def get_sheet(self, sheet_name):
         return self.doc.worksheet(sheet_name)
     
@@ -57,6 +61,7 @@ class SpreadsheetService:
     #####################
 
     def get_student_courses(self, email:str) -> list:
+        self.set_active_document(GOOGLE_SHEETS_MASTER_DOCUMENT_ID)
         students_sheet = self.get_sheet("students_roster")
         all_student_course_ids = students_sheet.get_all_records()
         student_course_ids = [course["COURSE_ID"] for course in all_student_course_ids if course["STUDENT_EMAIL"] == email]
@@ -69,12 +74,75 @@ class SpreadsheetService:
         return student_courses_info
 
 
+    def get_course_assignments(self, student_email:str, course_id:str) -> list:
+        #if coming from "courses" page, the active document will be "MASTER"
+        #we want to change that to the document ID of the specific course
+        start_time = time.time()
+        if self.doc.id == GOOGLE_SHEETS_MASTER_DOCUMENT_ID:
+            courses_sheet = self.get_sheet("courses")
+            courses_records = courses_sheet.get_all_records()
+        
+            course_document_id_list = [c["SHEETS_DOCUMENT_ID"] for c in courses_records if c["COURSE_ID"] == int(course_id)]
 
-    
-    def get_assignment_grade(self, username, assignment_name):
-        sheet = self.get_sheet(f"{assignment_name}-mjr") #> <class 'gspread.models.Worksheet'>
-        row_index = sheet.col_values(3).index(username) + 1
-        return sheet.row_values(row_index)
+            if len(course_document_id_list) == 0:
+                raise Exception("course not found...")
+                #TODO: handle within the route
+            if len(course_document_id_list) > 1:
+                raise Exception("course duplicate found...error")
+                #TODO: handle within the route
+            
+            self.set_active_document(course_document_id_list[0])
+
+        # now, get the assignments from the sheet
+        assignments_sheet = self.get_sheet("ASSIGNMENT_MASTER")
+        assignments_records = assignments_sheet.get_all_records()
+
+        
+        #now, iterate through each assignment sheet, find the grade corresponding with the email/netID
+        # ?? will this reach the sheets rate limit? especially for multiple assignments...
+        assignment_index = 0
+        for assignment in assignments_records:
+            sheet = self.get_sheet(assignment["SHEET_NAME"])
+            header_row_index = -1
+            
+            first_column_values = sheet.col_values(1)
+            for idx, value in enumerate(first_column_values, start=1):
+                if value == "Timestamp":
+                    header_row_index = idx
+                    break
+            
+            emails_column_index = -1
+            final_grade_column_index = -1
+
+            header_row_values = sheet.row_values(header_row_index)
+
+            for idx, value in enumerate(header_row_values, start=1):
+                if "email" in value.lower():
+                    emails_column_index = idx
+                elif "raw" in value.lower() and "score" in value.lower():
+                    final_grade_column_index = idx
+                
+                if emails_column_index > 0 and final_grade_column_index > 0:
+                    break
+
+            #now find the assignment grade
+            student_row_index = -1
+            for idx, email in enumerate(sheet.col_values(emails_column_index), start=1):
+                if email == student_email:
+                    student_row_index = idx
+                    break
+            try:
+                student_grade = sheet.cell(student_row_index, final_grade_column_index).value
+                assignments_records[assignment_index]["GRADE"] = student_grade
+            except Exception as e:
+                assignments_records[assignment_index]["GRADE"] = None
+
+            assignment_index += 1
+
+
+
+        print(assignments_records)
+        print(time.time() - start_time)
 
 
 
@@ -82,4 +150,4 @@ if __name__ == "__main__":
 
     ss = SpreadsheetService()
 
-    ss.get_student_courses("at2015@nyu.edu")
+    ss.get_course_assignments("st4505@nyu.edu", "12345")
--- web_app/routes/user_routes.py
+++ web_app/routes/user_routes.py
@@ -1,7 +1,7 @@
 
 from flask import Blueprint, render_template, flash, redirect, current_app, url_for, session, request #, jsonify
 
-from web_app.routes.wrappers import authenticated_route
+from web_app.routes.wrappers import student_authenticated_route
 
 user_routes = Blueprint("user_routes", __name__)
 
@@ -10,7 +10,7 @@ user_routes = Blueprint("user_routes", __name__)
 #
 
 @user_routes.route("/user/courses")
-@authenticated_route
+@student_authenticated_route
 def courses():
     print("USER COURSES...")
     current_user = session.get("current_user")
@@ -20,57 +20,12 @@ def courses():
 
     return render_template("courses.html", courses=courses)
 
-
-"""
-
-@user_routes.route("/user/orders")
-@authenticated_route
-def orders():
-    print("USER ORDERS...")
-    current_user = session.get("current_user")
-    service = current_app.config["SPREADSHEET_SERVICE"]
-    orders = service.get_user_orders(current_user["email"])
-    return render_template("user_orders.html", orders=orders)
-
-
-@user_routes.route("/user/orders/create", methods=["POST"])
-@authenticated_route
-def create_order():
-    print("CREATE USER ORDER...")
-
-    form_data = dict(request.form)
-    print("FORM DATA:", form_data)
-    product_id = form_data["product_id"]
-    product_name = form_data["product_name"]
-    product_price = form_data["product_price"]
-
-    current_user = session.get("current_user")
-    user_email = current_user["email"]
-
-    service = current_app.config["SPREADSHEET_SERVICE"]
-    try:
-        new_order = {
-            "user_email": user_email,
-            "product_id": int(product_id),
-            "product_name": product_name,
-            "product_price": float(product_price)
-        }
-        service.create_order(new_order)
-        flash(f"Order received!", "success")
-        return redirect("/user/orders")
-    except Exception as err:
-        print(err)
-        flash(f"Oops, something went wrong: {err}", "warning")
-        return redirect("/products")
-
- """
-
 #
 # USER PROFILE
 #
 
 @user_routes.route("/user/profile")
-@authenticated_route
+@student_authenticated_route
 def profile():
     print("USER PROFILE...")
     current_user = session.get("current_user")
